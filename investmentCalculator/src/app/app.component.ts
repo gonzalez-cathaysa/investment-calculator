7a1de93d9421c4f6bd38680907658fbb88e2ab20
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-06 19:50:21 +0200

    Passing Data from Parent to Child with inputs

--- investmentCalculator/src/app/app.component.ts
+++ investmentCalculator/src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [HeaderComponent, UserInputComponent]
+    imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
-  calculateInvestmentResults(data: InvestmentInput) {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } =
       data;
     const annualData = [];
@@ -33,6 +43,6 @@ export class AppComponent {
       });
     }
 
-    return annualData;
+    this.resultsData = annualData;
   }
 }
